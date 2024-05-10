5ee987fa2a9e0cf17d1b32580c3112f3331da36c
luisAlura <107197542+luisAlura@users.noreply.github.com>
2024-05-10 10:46:14 -0300

    10/05

--- main.js
+++ main.js
@@ -37,14 +37,17 @@ function calculaTempo (tempoObjetivo){
     horas %=24;
 
     if (tempoFinal>0){
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    return [dias, horas, minutos, segundos];
     } else {
-        return "Missão Completa"
+        return [0,0,0,0];
     }
     }
     function atualizaCronometro(){
         for(let i=0; i<contadores.length; i++){
-            contadores[i].textContent = calculaTempo(tempos[i]);
+            document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+            document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+            document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+            document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
         }
     }
     function comecaCronometro(){
